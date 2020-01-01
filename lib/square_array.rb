f456e7b86383a6148d5007bd5270fc01e1be6a60
Jurabek Tolmasov <tolmasovjurabek@gmail.com>
2020-01-01 13:32:00 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -3,7 +3,7 @@ def square_array(array)
   counter = 0
   
   while counter < array.length do
-    new-array << array[counter] ** 2
+    new_array << array[counter] ** 2
     couneter += 1
    new_array
  end
